58504892dcdaca639dabc3d81a02eba34af7cfb4
Sagar Patil <admin@Sagars-MacBook-Air.local>
2025-03-02 16:17:46 +0530

    updated backend url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.NODE_ENV === 'production'
+    ? 'https://dripfit-backend.vercel.app/' // Replace with your actual backend URL
+    : 'http://localhost:3001',
 });
 
 export default instance;
